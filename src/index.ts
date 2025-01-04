6d782e603bb623c4041834e7c36268ce547ca97b
icedlee337 <lee.e.norris@gmail.com>
2025-01-04 17:45:51 -0600

    chore: export refresh token service

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ export * from './lib/server-aws-cognito.module';
 
 export * from './lib/decorators/access-token-header.decorator';
 
+export * from './lib/services/cognito-refresh-token.service';
 export * from './lib/services/cognito-token-validator.service';
 
 export * from './lib/types/cognito-attribute.type';
